04ae8407c19dc3db1d20159a0134a1b3f7bf13de
ShubHH03 <mak.shubham2003@gmail.com>
2025-12-10 20:27:08 +0530

    feat: implement voting logic and reputation updates in POST endpoint

--- src/api/vote/route.ts
+++ src/api/vote/route.ts
@@ -1,19 +1,177 @@
-import { tablesDB } from "@/src/models/server/config";
+import {
+  answerCollection,
+  db,
+  questionCollection,
+  voteCollection,
+} from "../../models/name";
+import { tablesDB, users } from "../../models/server/config";
+import { UserPrefs } from "../../store/Auth";
 import { NextRequest, NextResponse } from "next/server";
+import { ID, Query } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
   try {
+    const { votedById, voteStatus, type, typeId } = await request.json();
 
-    // grab vote data
-    const {votedById, voteStatus, typeId,type } = await request.json();
+    const response = await tablesDB.listRows({
+      databaseId: db,
+      tableId: voteCollection,
+      queries: [
+        Query.equal("type", type),
+        Query.equal("typeId", typeId),
+        Query.equal("votedById", votedById),
+      ],
+    });
 
-    // const response = await tablesDB.
+    if (response.rows.length > 0) {
+      await tablesDB.deleteRow({
+        databaseId: db,
+        tableId: voteCollection,
+        rowId: response.rows[0].$id,
+      });
 
-  } catch (error: any) {
+      // Decrease the reputation of the question/answer author
+      const questionOrAnswer = await tablesDB.getRow({
+        databaseId: db,
+        tableId: type === "question" ? questionCollection : answerCollection,
+        rowId: typeId,
+      });
+
+      const authorPrefs = await users.getPrefs<UserPrefs>({
+        userId: questionOrAnswer.authorId,
+      });
+
+      await users.updatePrefs<UserPrefs>({
+        userId: questionOrAnswer.authorId,
+        prefs: {
+          reputation:
+            response.rows[0].voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) - 1
+              : Number(authorPrefs.reputation) + 1,
+        },
+      });
+    }
+
+    // that means prev vote does not exists or voteStatus changed
+    if (response.rows[0]?.voteStatus !== voteStatus) {
+      const doc = await tablesDB.createRow({
+        databaseId: db,
+        tableId: voteCollection,
+        rowId: ID.unique(),
+        data: {
+          type,
+          typeId,
+          voteStatus,
+          votedById,
+        },
+      });
+
+      // Increate/Decrease the reputation of the question/answer author accordingly
+      const questionOrAnswer = await tablesDB.getRow({
+        databaseId: db,
+        tableId: type === "question" ? questionCollection : answerCollection,
+        rowId: typeId,
+      });
+
+      const authorPrefs = await users.getPrefs<UserPrefs>({
+        userId: questionOrAnswer.authorId,
+      });
+
+      // if vote was present
+      if (response.rows[0]) {
+        await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+          reputation:
+            // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+            response.rows[0].voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) - 1
+              : Number(authorPrefs.reputation) + 1,
+        });
+      } else {
+        await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+          reputation:
+            // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+            voteStatus === "upvoted"
+              ? Number(authorPrefs.reputation) + 1
+              : Number(authorPrefs.reputation) - 1,
+        });
+      }
+
+      const [upvotes, downvotes] = await Promise.all([
+        tablesDB.listRows({
+          databaseId: db,
+          tableId: voteCollection,
+          queries: [
+            Query.equal("type", type),
+            Query.equal("typeId", typeId),
+            Query.equal("voteStatus", "upvoted"),
+            Query.equal("votedById", votedById),
+            Query.limit(1), // for optimization as we only need total
+          ],
+        }),
+        tablesDB.listRows({
+          databaseId: db,
+          tableId: voteCollection,
+          queries: [
+            Query.equal("type", type),
+            Query.equal("typeId", typeId),
+            Query.equal("voteStatus", "downvoted"),
+            Query.equal("votedById", votedById),
+            Query.limit(1), // for optimization as we only need total
+          ],
+        }),
+      ]);
+
+      return NextResponse.json(
+        {
+          data: { document: doc, voteResult: upvotes.total - downvotes.total },
+          message: response.rows[0] ? "Vote Status Updated" : "Voted",
+        },
+        {
+          status: 201,
+        }
+      );
+    }
+
+    const [upvotes, downvotes] = await Promise.all([
+      tablesDB.listRows({
+        databaseId: db,
+        tableId: voteCollection,
+        queries: [
+          Query.equal("type", type),
+          Query.equal("typeId", typeId),
+          Query.equal("voteStatus", "upvoted"),
+          Query.equal("votedById", votedById),
+          Query.limit(1), // for optimization as we only need total
+        ],
+      }),
+      tablesDB.listRows({
+        databaseId: db,
+        tableId: voteCollection,
+        queries: [
+          Query.equal("type", type),
+          Query.equal("typeId", typeId),
+          Query.equal("voteStatus", "downvoted"),
+          Query.equal("votedById", votedById),
+          Query.limit(1), // for optimization as we only need total
+        ],
+      }),
+    ]);
+
     return NextResponse.json(
       {
-        message: error?.message || "Error in voting",
+        data: {
+          document: null,
+          voteResult: upvotes.total - downvotes.total,
+        },
+        message: "Vote Withdrawn",
       },
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: error?.message || "Error deleting answer" },
       { status: error?.status || error?.code || 500 }
     );
   }
